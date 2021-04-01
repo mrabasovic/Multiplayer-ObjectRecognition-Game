a6c69a4c8b9627c31e455f592c820f1bc444c45f
mrabasovic <rabasovicmladen@gmail.com>
2021-04-01 22:21:19 +0200

    Initial Commit

---
+++ mobilnoracunarstvo/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  mobilnoracunarstvo
+//
+//  Created by mladen on 1.4.21..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var hostBtn: UIButton!
+    @IBOutlet weak var joinBtn: UIButton!
+    @IBOutlet weak var singleBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        srediDugmad()
+    }
+
+
+    func srediDugmad(){
+        hostBtn.backgroundColor = .clear
+        hostBtn.layer.cornerRadius = 5
+        hostBtn.layer.borderWidth = 1
+        hostBtn.layer.borderColor = UIColor.black.cgColor
+        hostBtn.layer
+    }
+}
+
